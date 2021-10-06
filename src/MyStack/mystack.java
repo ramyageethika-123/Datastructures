85f7f29753fba0ef34374616d40f4a95b0ba0917
Ramya Geethika <iamramyageethika@gmail.com>
2021-10-06 15:05:50 +0530

    Changed the stack file

--- src/MyStack/mystack.java
+++ src/MyStack/mystack.java
@@ -2,20 +2,19 @@ package MyStack;
 import java.util.*;
 public class mystack {
 	public static void main(String[] args) {
-
-			Queue<Integer> q = new LinkedList<>();
-		    for (int i = 0; i < 5; i++)
-		           q.add(i);
-		    // Display contents of the queue.
-		    System.out.println("Elements of queue "+ q);
-		    // To remove the head of queue.
-		    int removed_element = q.remove();
-		    System.out.println("removed element-"+ removed_element);
-		    System.out.println(q);
-		    // To view the head of queue
-		    int head = q.peek();
-		    System.out.println("head of queue-"+ head);
-		    int size = q.size();
-		    System.out.println("Size of queue-"+ size);
+		Stack<String> trail_stack = new Stack <String>();
+		boolean result = trail_stack.empty();
+		System.out.println("Is the stack empty? " + result);  
+		trail_stack.push("I");
+		trail_stack.push("you");
+		trail_stack.push("how");
+		trail_stack.push("go");
+		trail_stack.push("went");
+		System.out.println(trail_stack);
+		trail_stack.pop();
+		trail_stack.pop();
+		System.out.println("After pop operation" +trail_stack);
+		System.out.println(trail_stack.peek());
+		System.out.println(trail_stack.search("how"));
 	}
 }

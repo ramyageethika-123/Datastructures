f51db3d7dcfbbfeb23ba4b352db3654194e95cd6
Ramya Geethika <iamramyageethika@gmail.com>
2021-10-06 15:43:27 +0530

    Added a queue file

---
+++ src/MyStack/myqueue.java
@@ -0,0 +1,22 @@
+package MyStack;
+import java.util.*;
+public class myqueue {
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	Queue<Integer> q = new LinkedList<>();
+    for (int i = 0; i < 5; i++)
+           q.add(i);
+    // Display contents of the queue.
+    System.out.println("Elements of queue "+ q);
+    // To remove the head of queue.
+    int removed_element = q.remove();
+    System.out.println("removed element-"+ removed_element);
+    System.out.println(q);
+    // To view the head of queue
+    int head = q.peek();
+    System.out.println("head of queue-"+ head);
+    int size = q.size();
+    System.out.println("Size of queue-"+ size);
+	}
+}
+

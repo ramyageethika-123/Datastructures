268fe99e91347f2b1b14668d592950a71631716d
Ramya Geethika <iamramyageethika@gmail.com>
2021-10-10 20:07:59 +0530

    Just making new commit

--- src/MyStack/queue_operations.java
+++ src/MyStack/queue_operations.java
@@ -1,10 +1,14 @@
 package MyStack;
 
-public class queue {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.util.*;
+public class queue_operations {
+	public static void main(String args[]) {
+		Queue<Integer> Queue_operator= new LinkedList<Integer>();
+		Queue_operator.add(1);
+		Queue_operator.add(2);
+		Queue_operator.add(3);
+		System.out.println("Top element"+Queue_operator.peek());
+		System.out.println("Top element"+Queue_operator.poll());
+		System.out.println("Top element"+Queue_operator.peek());
 	}
-
-}
+}  
